ba8e9e4700311ffac746d6c7c80cf525fffee23d
Joseph Tongay <dirk.tongay@gmail.com>
2017-01-25 15:15:13 -0600

    active links work in the navbar

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("booyah");
+(function(doc){
+
+// window.onload = function(){
+//
+//
+// }
+
+var links = doc.querySelectorAll('.links')
+
+for(var i = 0; i < links.length; i++){
+  links[i].addEventListener('click', function(event){
+    for(var j = 0; j < links.length; j++){
+      links[j].className = ''
+    }
+    this.className = 'active-link';
+  })
+}
+
+
+})(document)
