4e5f570117cf6e44b25beb54686ed5894ee9fb6e
Joseph Tongay <dirk.tongay@gmail.com>
2017-01-25 18:18:35 -0600

    hover effects look ok, all click events go to the right place

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 (function(doc){
 
-// window.onload = function(){
-//
-//
-// }
+const links = doc.querySelectorAll('.links'),
+pokeStayImg = doc.getElementById('pokestay-img'),
+digiDexImg = doc.getElementById('digidex-img'),
+raspiImg = doc.getElementById('raspi-img'),
+introSection = doc.getElementById('intro-section'),
+iconsSection = doc.getElementById('icons-section'),
+pokeSection = doc.getElementById('poke-section'),
+digiDexSection = doc.getElementById('digidex-section'),
+raspiSection = doc.getElementById('raspi-section'),
+powerSection = doc.getElementById('power-section')
 
-var links = doc.querySelectorAll('.links')
-
+//click events to show active links color only and make the rest go back
 for(var i = 0; i < links.length; i++){
   links[i].addEventListener('click', function(event){
     for(var j = 0; j < links.length; j++){
@@ -15,6 +20,27 @@ for(var i = 0; i < links.length; i++){
     this.className = 'active-link';
   })
 }
+
+//click events to go to a page
+pokeStayImg.addEventListener('click', function(event){
+  introSection.style.display = 'none';
+  iconsSection.style.display = 'none';
+  pokeSection.style.display = 'block';
+})
+
+raspiImg.addEventListener('click', function(){
+  introSection.style.display = 'none';
+  iconsSection.style.display = 'none';
+  raspiSection.style.display = 'block';
+})
+
+digiDexImg.addEventListener('click', function(){
+  introSection.style.display = 'none';
+  iconsSection.style.display = 'none';
+  digiDexSection.style.display = 'block';
+})
+
+
 
 
 })(document)
